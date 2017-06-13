ee75f23356c54a03cd546a74adf6ee4639a13ebe
Kashif <kashif@Kashifs-MacBook-Air.local>
2017-06-13 13:50:08 +0530

    I modified myjs.js to add comment

---
+++ myjs.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$(".box").hover(function() {
+		/* Stuff to do when the mouse enters the element */
+		//$(this).css("width","400px");
+		$(this).animate({width:"400px"}, 1000)
+	}, function() {
+		/* Stuff to do when the mouse leaves the element */
+		$(this).animate({width:"300px"}, 200)
+	});
+
+
+	$("body").click(function(){
+		$(".box").stop();
+	});
+
+
+});
